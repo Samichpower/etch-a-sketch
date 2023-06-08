ec8b20349669c77a0157342432f4b474ba19a63e
Austin R <samichpower@yahoo.com>
2023-06-08 17:01:21 -0500

    Sets the height and width dynamically

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const gridContainer = document.querySelector('.grid-container')
+console.log(gridContainer.clientWidth);
 
 for (let i = 0; i < 256; i++) {
   const newSquare = document.createElement('div');
   newSquare.className = 'grid-item';
+  newSquare.style.width = `${gridContainer.clientWidth / 16}px`;
+  newSquare.style.height = newSquare.style.width;
   gridContainer.appendChild(newSquare);
 }
